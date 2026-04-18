0308f8621f9a6fb71fdad0d930d1dbbd6901052d
Sandro Offermans <sandro.offermans@beeone.nl>
2026-04-18 17:35:59 +0200

    Created successful tests for MovingAverageCrossStrategy

---
+++ CryptoBot.Tests/MovingAverageCrossStrategyTest.cs
@@ -0,0 +1,66 @@
+﻿using CryptoBot.Domain.Enums;
+using CryptoBot.Domain.Models;
+using CryptoBot.Domain.Strategies;
+
+namespace CryptoBot.Tests;
+
+public class MovingAverageCrossStrategyTest
+{
+    [Fact]
+    public void ShortSMACrossesAboveLongSMA()
+    {
+        var strategy = new MovingAverageCrossStrategy(3, 5);
+        var candleList = new List<Candle>
+        {
+            new Candle { Close = 3m },
+            new Candle { Close = 4m },
+            new Candle { Close = 3m },
+            new Candle { Close = 4m },
+            new Candle { Close = 3m },
+            new Candle { Close = 15m },
+        };
+        var signal = strategy.Evaluate(candleList);
+        Assert.Equal(Signal.Buy, signal);
+    }
+
+    [Fact]
+    public void ShortSMACrossesBelowLongSMA()
+    {
+        var strategy = new MovingAverageCrossStrategy(3, 5);
+        var candleList = new List<Candle>
+        {
+            new Candle { Close = 4m },
+            new Candle { Close = 3m },
+            new Candle { Close = 15m },
+            new Candle { Close = 3m },
+            new Candle { Close = 4m },
+            new Candle { Close = 3m },
+        };
+        var signal = strategy.Evaluate(candleList);
+        Assert.Equal(Signal.Sell, signal);
+    }
+
+    [Fact]
+    public void FewerCandlesThanLongPeriod()
+    {
+        var strategy = new MovingAverageCrossStrategy(3, 5);
+        var candleList = new List<Candle>
+        {
+            new Candle { Close = 4m },
+            new Candle { Close = 3m },
+            new Candle { Close = 15m },
+            new Candle { Close = 3m },
+        };
+        var signal = strategy.Evaluate(candleList);
+        Assert.Equal(Signal.Hold, signal);
+    }
+
+    [Fact]
+    public void EmptyListCandles()
+    {
+        var strategy = new MovingAverageCrossStrategy(3, 5);
+        var candleList = new List<Candle>();
+        var signal = strategy.Evaluate(candleList);
+        Assert.Equal(Signal.Hold, signal);
+    }
+}
