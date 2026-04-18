15cf808b1bd1c93fe9c7385525dc1c54858bd008
Sandro Offermans <sandro.offermans@beeone.nl>
2026-04-18 19:33:07 +0200

    Bitvavo HMAC authentication and created successful test

---
+++ CryptoBot.Bitvavo/BitvavoAuth.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoBot.Bitvavo;
+
+public class BitvavoAuth
+{
+    public static string CreateSignature(string secret, long timestamp, string method, string path, string body)
+    {
+        var concatenatedString = GetConcatenatedString(timestamp, method, path, body);
+
+        var key = Encoding.UTF8.GetBytes(secret);
+        var message = Encoding.UTF8.GetBytes(concatenatedString);
+
+        using var hmac = new HMACSHA256(key);
+        var hash = hmac.ComputeHash(message);
+
+        // Convert to lowercase hex string
+        var hexString = Convert.ToHexString(hash).ToLower();
+
+        return hexString;
+    }
+
+    public static string GetConcatenatedString(long timestamp, string method,  string path, string? body)
+        => string.Concat(timestamp, method, path, body);
+
+    public static long GetTimeStamp() => DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();    
+}
---
+++ CryptoBot.Tests/BitvavoAuthTest.cs
@@ -0,0 +1,22 @@
+﻿using CryptoBot.Bitvavo;
+
+namespace CryptoBot.Tests;
+
+public class BitvavoAuthTest
+{
+    [Fact]
+    public void CreateHMACSignature()
+    {
+        var secret = "test-secret";
+
+        // 1776532140000
+        var timestamp = new DateTimeOffset(2026, 4, 18, 17, 9, 0, TimeSpan.Zero).ToUnixTimeMilliseconds();
+
+        var method = "POST";
+        var path = "test-path";
+        var body = "thisisatest";
+
+        var signature = BitvavoAuth.CreateSignature(secret, timestamp, method, path, body);
+        Assert.Equal("5e75f7d6110def0ba3ff6915d57651694179ca2eaa82b2af13875ba1231ca909", signature);
+    }
+}
